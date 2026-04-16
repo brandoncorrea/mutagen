6e195d485b42cee6f67206ee45f05b319bba7499
Brandon Correa <me@bwawan.com>
2026-04-16 03:11:19 -0400

    simplify mutator import

--- mutagen.config.js
+++ mutagen.config.js
@@ -1,8 +1,8 @@
-import { javascript as astMutators } from './src/core/ast-mutators.js'
+import { javascript } from './src/core/ast-mutators.js'
 import { createVitestRunner } from './src/runners/vitest.js'
 
 export default {
-  mutators: astMutators,
+  mutators: javascript,
   include: ['src/**/*.js'],
   createRunner: (sourceFile, opts = {}) => createVitestRunner(sourceFile, {
     config: 'vitest.config.js',
